2d4892fbbc9d587915e1a3e5dc93baa54e916acc
Mohammad AbuShady <coalwater5@gmail.com>
2015-01-24 18:24:48 +0200

    Let url factory use localhost for original_urls

--- spec/factories/urls.rb
+++ spec/factories/urls.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :url do
     key {Faker::Lorem.characters(10)}
-    original_url  {Faker::Internet.url('http://localhost')}
+    original_url  {Faker::Internet.url('localhost:3000')}
  end
 
 end
