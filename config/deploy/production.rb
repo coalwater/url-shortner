36bd721040539caaa50cfa6a8fb954399054f01d
Mohammad AbuShady <coalwater5@gmail.com>
2015-01-25 21:17:38 +0200

    Stupid swap mistake

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,8 +12,8 @@ set :puma_workers, 1
 set :puma_bind, -> {"unix://#{shared_path}/tmp/sockets/#{fetch(:application)}_puma.sock"}
 set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
 set :puma_pid, -> {"#{shared_path}/tmp/pids/puma.pid"}
-set :puma_access_log, -> {"#{release_path}/log/puma.error.log"}
-set :puma_error_log, -> {"#{release_path}/log/puma.access.log"}
+set :puma_access_log, -> {"#{release_path}/log/puma.access.log"}
+set :puma_error_log, -> {"#{release_path}/log/puma.error.log"}
 set :puma_preload_app, true
 set :puma_worker_timeout, 30
 set :puma_role, :app
