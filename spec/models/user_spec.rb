39a351040112b6a36f8935d50426492536a43b8d
Mohammad AbuShady <coalwater5@gmail.com>
2015-01-26 23:41:28 +0200

    Simple user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 describe User, type: :model do
+    describe 'validations' do
+      it 'is invalid without an email' do
+        expect(build :user, email: nil).not_to be_valid
+      end
+      it 'is invalid without a password' do
+        expect(build :user, password: nil).not_to be_valid
+      end
+    end
 
 end
