85c807f852fc84b664eadfeeee42feff872d7f71
Mohammad AbuShady <coalwater5@gmail.com>
2015-02-16 22:43:43 +0200

    Add flash messages in the layout with it's helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def flash_messages
+    ''.tap do |flashes|
+      flashes << content_tag(:div, flash.alert, class: 'alert alert-danger') if flash.alert
+      flashes << content_tag(:div, flash.notice, class: 'alert alert-info') if flash.notice
+    end.html_safe
+  end
 end
